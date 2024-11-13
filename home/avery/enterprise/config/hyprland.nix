a1d9de6d198337a6c384308eb547b80b4ae4c8e8
Avery Keuben <avery1516@gmail.com>
2024-11-13 13:59:41 -0700

    Swap monitors hyprland

--- home/avery/enterprise/config/hyprland.nix
+++ home/avery/enterprise/config/hyprland.nix
@@ -1,8 +1,8 @@
 { ... }: {
     wayland.windowManager.hyprland.settings = {
         monitor = [
-            "DP-2, 2560x1440@165, 0x0, 1"
-            "DP-1, 2560x1440@165, 2560x0, 1"
+            "DP-1, 2560x1440@165, 0x0, 1"
+            "DP-2, 2560x1440@165, 2560x0, 1"
         ];
     };
 }
