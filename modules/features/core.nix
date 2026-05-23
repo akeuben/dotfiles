37fa5e14c7e569e1d5a8867e5eaccdd53fd3cde1
Avery Keuben <avery1516@gmail.com>
2026-05-23 15:57:14 -0600

    fix core naming

--- modules/features/core.nix
+++ modules/features/core.nix
@@ -1,7 +1,7 @@
 {self, inputs, ...}: {
     flake.nixos-modules.features.core = { config, lib, pkgs, modulesPath, ... }: {
         imports = with self.nixos-modules; [
-            core-packages 
+            corepackages 
             hello
             network 
         ];
